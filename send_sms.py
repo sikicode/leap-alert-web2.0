861116d8fb2c16b39fb752f4f6525434ba0aa2e5
JustinHendricks <47921340+JustinHendricks@users.noreply.github.com>
2019-09-15 05:02:54 -0400

    Censored authorization items

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACba3bd47c9109be93ea7d9dc7a25a3af9'
-auth_token = '8e78e64aa7ef5972f7f223af85f3b3b6'
+account_sid = 'ACba3bd47c*****93ea7d9dc7a25****'
+auth_token = '8e78e64aa7ef5******23af85f3b3**'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
